52085c093e1626e0dc64fd3c08999486ec29cd69
marklienista <marklienista@gmail.com>
2026-09-21 15:19:28 -0300

    Atualiza versão dos módulos do Som da Turma

--- barulho/control-sync.js
+++ barulho/control-sync.js
@@ -7,10 +7,10 @@
   };
 
   const core=document.createElement('script');
-  core.src='control-core.js?v=20260921-4';
+  core.src='control-core.js?v=20260921-5';
   core.onload=()=>{
     const profiles=document.createElement('script');
-    profiles.src='profiles.js?v=20260921-4';
+    profiles.src='profiles.js?v=20260921-5';
     document.head.appendChild(profiles);
   };
   document.head.appendChild(core);
