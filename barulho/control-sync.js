f437a063541ee0fe5f0621e3cae3554d7af326ed
marklienista <marklienista@gmail.com>
2026-09-21 15:17:03 -0300

    Atualiza cache-busting dos módulos do Som da Turma

--- barulho/control-sync.js
+++ barulho/control-sync.js
@@ -7,10 +7,10 @@
   };
 
   const core=document.createElement('script');
-  core.src='control-core.js';
+  core.src='control-core.js?v=20260921-4';
   core.onload=()=>{
     const profiles=document.createElement('script');
-    profiles.src='profiles.js';
+    profiles.src='profiles.js?v=20260921-4';
     document.head.appendChild(profiles);
   };
   document.head.appendChild(core);
